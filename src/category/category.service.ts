508cf0489e20b103424e5925bf596ccbb1a25aa7
Сергей Похабов <itigertechno@gmail.com>
2024-02-10 03:12:43 +0600

    fox

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -20,7 +20,7 @@ export class CategoryService {
 
   findAll(): Promise<Category[]> {
     return this.categoryRepository.find({
-      relations: ['meditation', 'audios', 'audios.voice']
+      relations: ['meditations', 'audios', 'audios.voice']
     });
   }
 
